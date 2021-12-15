5b4a9dd820aa9fd55723375de2c3e2fbec80051e
Jenya Rusinova <eugeniya.rusinova@gmail.com>
2021-12-15 11:08:40 -0800

    Add a question button rendered conditionally

--- client/src/components/QnAcomponents/AddQuestion.jsx
+++ client/src/components/QnAcomponents/AddQuestion.jsx
@@ -3,12 +3,43 @@ import React from 'react';
 class AddQuestion extends React.Component {
   constructor(props){
     super(props);
+    this.state = {
+      isModalShown: false,
+      isAddButtonShown: true,
+      product_id: 1,
+      question_body:'',
+      nickname:'',
+      email:''
+    }
+    this.clickOnAddQuestion = this.clickOnAddQuestion.bind(this);
+  }
+
+  clickOnAddQuestion(){
+    this.setState({
+      isModalShown: true,
+      isAddButtonShown: false
+    })
   }
 
 
   render(){
+    let modal;
+    let questionButton;
+    if(this.state.isModalShown){
+      modal = <div>This is modal window</div>
+    } else {
+      modal = <div></div>
+    }
+    if(this.state.isAddButtonShown){
+      questionButton =  <button onClick={()=>this.clickOnAddQuestion()}>Add a new question</button>
+    } else {
+      questionButton = <div></div>
+    }
     return(
-      <button>Add a new question</button>
+      <div>
+        {questionButton}
+        {modal}
+      </div>
     )
   }
 }
