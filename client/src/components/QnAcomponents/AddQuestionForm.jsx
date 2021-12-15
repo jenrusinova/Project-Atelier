04a43433a4f7c6bfe8dcfb3a0fd093c69f6c227d
Jenya Rusinova <eugeniya.rusinova@gmail.com>
2021-12-15 11:14:03 -0800

    Created class component

---
+++ client/src/components/QnAcomponents/AddQuestionForm.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class AddQuestionForm extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+
+  render(){
+    return(
+      <div>This is form to add a new question</div>
+    )
+  }
+}
+
+export default AddQuestionForm;
